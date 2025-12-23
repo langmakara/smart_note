857d20011270f34e03e622ad1b9c123157692e9a
langmakara <langmakara122@gmail.com>
2025-12-23 20:35:32 +0700

    Remove commented title from AppBar and add notification.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        //title: Text('Home'),
         leading: IconButton(
           icon: Icon(Icons.menu),
           onPressed: () {
