feef550ef16e001c83c09e07f44195984ecb70ef
langmakara <langmakara122@gmail.com>
2025-12-24 16:14:27 +0700

    you not me

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());  //you not me
+  runApp(const MyApp());  //you not me agent
 }
 
 class MyApp extends StatelessWidget {
